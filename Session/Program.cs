dad886973409f6bb7a67a3920e1e4ffefd319fb6
arhangel2i <arhangel2i@mail.ru>
2021-11-29 12:41:58 +0300

    create session

---
+++ Session/Program.cs
@@ -0,0 +1,33 @@
+﻿//Выполнение заданий
+//1
+Razd();
+JobTask1();
+Razd();
+//2
+JobTask2();
+Razd();
+//3
+JobTask3();
+Razd();
+
+//перечень функций
+//разделитель
+void Razd()
+{
+    Console.WriteLine("----------------------------");
+}
+//задание1
+void JobTask1()
+{
+    Console.WriteLine("     Не выполнено");
+}
+//задание2
+void JobTask2()
+{
+    Console.WriteLine("     Не выполнено";   
+}
+//задание3
+void JobTask3()
+{
+    Console.WriteLine("     Не выполнено");
+}
